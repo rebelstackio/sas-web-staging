f58cb746860a74020708bb4903e68dd0d111923e
Javier <jegj57@gmail.com>
2017-04-24 16:53:11 -0500

    Add more tours informations

---
+++ app/data/tours/tours-cuzco.en.js
@@ -0,0 +1,88 @@
+export default {
+		title:'Cuzco',
+		subtitle: `Cuzco (or Cusco) is the oldest city of South America still alive where people conserve the official language of the Incas.`,
+		description: `Formerly named Cuzco by Spanish Conquistadors, the name originates from the Quechua the phrase 'qusqu wanka' ('Rock of the owl').
+This well-preserved historic capital of the Inca empire is where you'll find sacred sites, temples and huge fortresses. Cuzco was the epicenter of Inca ceremonies and also the launching pad for Spanish colonization and the spread of Christianity throughout the Andean world. This is evidenced by a proliferation of colonial christian churches, convents, monasteries and cathedrals - largley built atop former Inca sites.
+Today, Cuzco is a World Heritage site declared by Unesco and the major tourist destination in South America. Tourists come from all over the world to visit Cuzco and surrounding sites like Machu Piccu, Sacsayhuanman, Qenko, Puca Pucara and Tambo Machay. Popular activities among tourists include trekking, rafting, bird watching, culinary tours, shamanic ceremonies and shopping for arts hand-crafted from gold, silver, textile, ceramic.`,
+		url_image: 'images/tours/cuzco/cusco_3_t.jpg',
+		tours:[
+			{
+				title:'Cuzco City Tour',
+				subtitle:'Explore the city of Cuzco and surrounding and discover the best sites of ancient Inca and Spanish colonial culture ',
+				url_image:'images/tours/cuzco/cusco_4_t.jpg',
+				description:`Qoricancha: One of the most regarded churches in the city, the "gold room" was a sacred place where one could worship "Inti", the most revered Inca god. Entered only by fasting, barefoot and with a load on back as a sign of humility.
+Sacsayhuaman: on of the most amazing Incan constructions and especially regarded by tourists, according to the latest hypothesis says it was an important ceremonial center and not a fortress as was previously though. A massive and spectacular building built with enormous carved rocks with incredible precision. Along with Machu Picchu and Choquequirao, Sacsayhuaman is without doubht one of the greatest architectural works of Tahuantinsuyo. It's Quechua name means "satisfied falcon".
+Other archaeologically important sites like Qenko, Puca, Pucara, Tambo Machay, and Cuzco city.`,
+				additionalData: {
+					include: [
+						'Pick up from your hotel in Cuzco',
+						'Transportation',
+						'All entrance fees.',
+						'English speaking guide.',
+					],
+					notInclude: [
+						'Extras & gratuities',
+					],
+					duration: {
+						subtitle:'4 hours (approx.)',
+						data:[
+							'Departs daily at 1:30pm approximately',
+						]
+					},
+					recommendations: [
+						'Bring a jacket or fleece for cool afternoons during winter',
+						'A rain poncho in case of rain',
+					],
+					price: {
+						currency:'$',
+						ammount:60,
+						data:[
+							'A minimum of 2 participants are required'
+						]
+					}
+				}
+			},
+			{
+				title:'Machu Picchu Day Trip',
+				subtitle:'Enjoy a full day trip to breathtaking Machu Picchu. Discover the famous Inca city and learn about the ancient civilization that once thrived there.',
+				url_image:'images/tours/cuzco/machu_picchu_3_t.jpg',
+				description:`This tour is suited for first-time visitors, families (non-strenuous hiking) and individual travelers and is a once-in-a-lifetime experience - a must-do when visiting Peru!
+From Cusco, you'll be transferred to the railway station to enjoy your 2-hour scenic rail tour to Aguas Calientes. You have the option to upgrade to the The Vistadome panoramic rail service which is the most comfortable way to journey to Machu Picchu.
+The renovated Vistadome railcars have beautiful panoramic windows to make the most of the spectacular scenery and allow for fantastic photo opportunities. Delicious meals as well as hot and cold refreshments are available on the way.
+Once we have arrived at Aguas Calientes station we'll board a bus and head up to the ruins of the "sacred city" known as Machu Picchu. Your expert guide will walk with you through the main streets of these amazing ruins. Stroll through town squares and climb delicately carved stone stairways.
+Machu Picchu holds a deep spirituality in it's buildings and surrounding formations - we guarantee you ample time to embrace this magnificent wonder and steep in it's ancient glory. Hiking these ruins is sure to work up an appetite so there will be a buffet lunch waiting for you. In the afternoon you'll return to the train station in Aguas Calientes for the trip back to Cusco.`,
+				additionalData: {
+					include: [
+						'Day trip to Machu Picchu from Cuzco',
+						'Scenic rail tour',
+						'Upgrade option to "Vistadome" panaramic rail service',
+						'Guided tour of the sacred city',
+						'Pick up from your hotel in Cuzco',
+						'All entrance fees, train and bus tickets.',
+						'English speaking guide.',
+					],
+					notInclude: [
+						'Extras & gratuities.'
+					],
+					duration: {
+						subtitle:'14 hours (approx.)',
+						data:[
+							'Pickup daily (from your hotel in Cuzco) at 5:30am approx.'
+						]
+					},
+					recommendations: [
+						'Bring a jacket or fleece for cool afternoons during winter',
+						'A rain poncho in case of rain',
+					],
+					price: {
+						currency:'$',
+						ammount:330,
+						data:[
+
+						]
+					}
+				}
+			},
+
+		]
+}
