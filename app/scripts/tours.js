3f5e772fc3519eb9a63e2b861278d6ed5a01143b
Edinson Nuñez More <nmedinson@gmail.com>
2017-04-27 12:26:54 -0500

    Add language vars in tours.js

--- app/scripts/tours.js
+++ app/scripts/tours.js
@@ -1,7 +1,8 @@
 import tpl from '../tpl/partials/tours-page.njk';
 let content = document.querySelector('#page-content');
 let dataRef = content.getAttribute('data-ref');
-System.import(`../data/tours/${dataRef}.en.js`).then(function(m) {
+let language = 'en';//capture param from url
+System.import(`../data/tours/${dataRef}.${url}.js`).then(function(m) {
   console.log(m);
   let data = m.default;
   var html = tpl.render({ data:  data });
