7e3b3500f7d3d633328a61e42bdda02e5bfccfe0
Javier <jegj57@gmail.com>
2017-05-02 16:34:40 -0500

    Add translate for articles Add rebelchat connection to rebelchat.io

---
+++ app/data/articles/articles.es.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		title:'Lineas de Nazca desde Pisco',
+		subtitle:"*Nuevo* Tours de las líneas de Nasca con vuelos regulares desde el aeropuerto renovado de Pisco",
+		blockquote:`"Antes, la única manera de ver las líneas de Nasca era viajar a Nasca - viajando en autobús. Pero ahora volamos desde Pisco y los turistas lo prefieren."`,
+		blockquoteAuthor:"",
+		author:'Rein Petersen',
+		publishDate:'2014-02-26',
+		description:'Llegar a Nasca para ver estas enigmáticas líneas de Nazca solía ser un esfuerzo grande y requería mucho tiempo. Al menos que usted quisiera rentar su propio vuelo privado desde Lima, el acceso al público se limitaba en su mayoría en coche o autobús, que tomaba mucho tiempo (12-14 horas ida y vuelta) viaje desde Lima.',
+		content:
+				`Llegar a Nasca para ver estas enigmáticas líneas de Nazca solía ser un esfuerzo grande y requería mucho tiempo. Al menos que usted quisiera rentar su propio vuelo privado desde Lima, el acceso al público se limitaba en su mayoría en coche o autobús, que tomaba mucho tiempo (12-14 horas ida y vuelta) viaje desde Lima.
+				"El sentimiento común transmitida por los visitantes a las Lineas de Nasca era una frustración que tomaba mucho tiempo el trayecto de llegar al destino para un sobrevuelo de 40 minutos", explica Lucio - un guía veterano de Perú con más de 20 años experiencia.
+				Mientras que el enigma que es las Líneas de Nasca es tan poderosa, para los viajeros que se reduce a la realidad de cómo quieren gastar su precioso tiempo de vacaciones - disfrutando de ese momento repleto de maravillas y actividades o hacinados en autobuses para largos períodos de tiempo.
+				Por suerte, las enigmáticas Líneas de Nasca están ahora disponibles a los tiempos limitados. Lucio continúa, "realizando vuelos desde el nuevo aeropuerto de Pisco es mucho más fácil El aeropuerto se encuentra a 15 minutos en coche de Paracas o un vuelo de 20 minutos de Lima. Ahorra todo un día de viaje que se puede utilizar para disfrutar otros attraciones que Paracas ofrece"`
+
+	}
+];
