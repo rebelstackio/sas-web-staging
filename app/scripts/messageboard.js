42b9cbba08566230c8a120a4f05d9f290a8f4c8a
Pablo Limo <limopablo@gmail.com>
2017-10-10 15:19:04 -0500

    progress in feature30

--- app/scripts/messageboard.js
+++ app/scripts/messageboard.js
@@ -1,6 +1,5 @@
 // This import loads the firebase namespace along with all its type information.
 import * as firebase from 'firebase/app';
-
 // These imports load individual services into the firebase namespace.
 import 'firebase/auth';
 import 'firebase/database';
@@ -14,10 +13,21 @@ var config = {
 };
 firebase.initializeApp(config);
 
-const language	= localStorage['lng'] || 'en' ;
-
 //Load messages from firebase
 var messages = firebase.database().ref('messages');
 messages.on('value', function(snapshot) {
     console.log(snapshot.val());
+var arr = snapshot.val();
+for (var i = 0; i < arr; i++) {
+    console.log(arr[i]);
+}
+});
+const language	= localStorage['lng'] || 'en' ;
+
+let board = document.querySelector('#page-content');
+System.import(`../tpl/partials/messages.${language}.njk`).then(function(m) {
+	console.log(m);
+	let tpl = m;
+	var html = tpl.render({});
+    board.innerHTML = html
 });
