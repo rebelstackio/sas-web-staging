f7af270662c5cb9aea474097962a7b3637126d65
Pablo Limo <limopablo@gmail.com>
2017-10-10 16:42:25 -0500

    backoffice progress

--- app/scripts/messageboard.js
+++ app/scripts/messageboard.js
@@ -1,5 +1,6 @@
 // This import loads the firebase namespace along with all its type information.
 import * as firebase from 'firebase/app';
+
 // These imports load individual services into the firebase namespace.
 import 'firebase/auth';
 import 'firebase/database';
@@ -13,16 +14,49 @@ var config = {
 };
 firebase.initializeApp(config);
 
+const language	= localStorage['lng'] || 'en' ;
+
 //Load messages from firebase
 var messages = firebase.database().ref('messages');
 messages.on('value', function(snapshot) {
-    console.log(snapshot.val());
-var arr = snapshot.val();
-for (var i = 0; i < arr; i++) {
-    console.log(arr[i]);
-}
+	snapshot.forEach(function(childSnapshot) {
+		var key = childSnapshot.key;
+		var childData = childSnapshot.val();
+		console.log(key);
+        console.log(childData);
+
+        var dte = new Date(childData.timestamp*1000);
+        var name = childData.name;
+        var email = childData.email;
+        var message = childData.message;
+
+        var title = document.createElement("div");
+        title.style.height = "30px";
+        title.style.background = "rgba(0,0,0,0.4)";
+        title.style.color = "white";
+        title.innerHTML = " On "+ dte + " " + name + "/" + email + " wrote: ";
+
+        var msg = document.createElement("div");
+        msg.style.height = "80px";
+        msg.style.background = "rgba(0,0,0,0.4)";
+        msg.style.color = "white";
+
+        var messagearea = document.createElement("div");
+        messagearea.style.height = "60px";
+        messagearea.style.background = "rgba(255,255,255,0.9)";
+        messagearea.style.color = "rgba(0,0,0,0.4)";
+        messagearea.style.margin = "0 15px";
+        messagearea.style.padding = "5px 5px";
+        messagearea.innerHTML = message;
+
+        msg.appendChild(messagearea);
+
+        var item = document.createElement('li');
+        item.appendChild(title);
+        item.appendChild(msg);
+        document.getElementById("message-list").appendChild(item);
+	});
 });
-const language	= localStorage['lng'] || 'en' ;
 
 let board = document.querySelector('#page-content');
 System.import(`../tpl/partials/messages.${language}.njk`).then(function(m) {
