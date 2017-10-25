227065fad7fa455b089142ec9f869f82208c93bc
Pablo <pablo@applied.pe>
2017-10-25 15:31:00 -0500

    static footer added on mobile

--- app/scripts/footer.js
+++ app/scripts/footer.js
@@ -2,9 +2,11 @@ import { getPageLanguage } from './utils';
 
 const language	= getPageLanguage('lng') || 'en' ;
 let footer = document.querySelector('#footer');
+let fmobile = document.querySelector("#footer-mobile");
 System.import(`../tpl/partials/footer.${language}.njk`).then(function(m) {
 	console.log(m);
 	let tpl = m;
 	var html = tpl.render({});
 	footer.innerHTML = html;
+	fmobile.innerHTML = html;
 });
