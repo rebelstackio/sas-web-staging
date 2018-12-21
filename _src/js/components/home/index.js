22c92c406fc207956054e464a298b40bca53c288
Osmar Reyes <osmarreyesst@gmail.com>
2018-12-21 00:33:12 -0500

    change of some text, added all the images && added style

--- _src/js/components/home/index.js
+++ _src/js/components/home/index.js
@@ -2,23 +2,38 @@ import { MetaComponent } from '@rebelstack-io/metaflux';
 import './index.css';
 
 class Home extends MetaComponent {
-	/**
-	 * MetaComponent constructor needs storage.
-	 */
+
 	constructor () {
 		super();
 		this.createArticles = this.createArticles.bind(this);
+		this.createAticlesImg = this.createAticlesImg.bind(this);
 	}
 	// eslint-disable-next-line class-method-use-this
 	render () {
-		const content = document.createElement('div');
-		this.articlesArea = document.createElement('div');
+		this.content = document.createElement('div');
+		this.content.className = 'text-content';
 		this.banners = document.createElement('div');
-		content.append(this.articlesArea, this.banners);
+		this.content.append(this.articlesArea, this.banners);
 		this.createArticles();
-		return content;
+		this.createAticlesImg();
+		return this.content;
 	}
-
+	/**
+	 * create the images for each article
+	 */
+	createAticlesImg () {
+		const {lastNews} = global.storage.getState().Main;
+		lastNews.forEach(element => {
+			const boxImg = document.createElement('div');
+			boxImg.className = 'box-img';
+			const img = document.createElement('img');
+			img.setAttribute('src', element.url_image)
+			this.content.appendChild()
+		})
+	}
+	/**
+	 * create each article
+	 */
 	createArticles () {
 		const {lastNews} = global.storage.getState().Main;
 		lastNews.forEach(element => {
@@ -28,10 +43,15 @@ class Home extends MetaComponent {
 			this.getMultyLangText(element.title, articleBox, 'div')
 			articleBox.appendChild(articleAuthor)
 			this.getMultyLangText(element.description, articleBox, 'p')
-			this.articlesArea.appendChild(articleBox);
+			this.content.appendChild(articleBox);
 		});
 	}
-
+	/**
+	 * create the html tags in both languages for the articles text
+	 * @param {Object} data 
+	 * @param {HTMLElement} box 
+	 * @param {String} type 
+	 */
 	getMultyLangText (data, box, type) {
 		Object.keys(data).forEach(k => {
 			const el = document.createElement(type)
