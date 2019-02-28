de8744d774289a15c9d5be74154768179dc01d14
samo1811 <47160691+samo1811@users.noreply.github.com>
2019-02-27 22:39:41 -0500

    Update tours-iquitos.es.js

--- app/data/tours/tours-iquitos.es.js
+++ app/data/tours/tours-iquitos.es.js
@@ -2,7 +2,7 @@ export default {
 		title:'Selva Tropical Peruana',
 		subtitle: `La selva amazónica peruana es una de las zonas con mayor diversidad biológica de la tierra`,
 		description: `Como nación, Perú tiene el mayor número de especies de aves en el mundo y el tercer mayor número de mamíferos, el 44% de las especies de aves y 63% de las especies de mamíferos que habitan la Amazonía peruana. Perú también tiene un alto número de especies de mariposas, orquídeas y otros organismos`,
-		url_image: 'images/tours/tambopata/iquitos_3_t.jpg',
+		url_image: 'https://res.cloudinary.com/dlia7phdx/image/upload/v1551324785/SAS/Reservas.jpg',
 		tab_title: "Tours Iquitos",
 		tours:[
 			{
@@ -10,7 +10,7 @@ export default {
 				title:'Tambopata',
 				shortTitle:'Tambopata',
 				subtitle:'Afamado en la revista National Geographic, el Tambopata Research Center es, probablemente, los albergues en la selva más remotas de América del Sur y ofrece a los aventureros una experiencia auténtica en la selva peruana',
-				url_image:'images/tours/tambopata/macaw_1_portrait.jpg',
+				url_image:'https://res.cloudinary.com/dlia7phdx/image/upload/v1551324785/SAS/Tambopata.jpg',
 				description:`El río Tambopata es uno de los hábitat exclusivo de las aves y los mamíferos. La flora en la reserva nacional es bastante típica del suroeste de la Cuenca Amazónica. El río Heath y llanuras de los alrededores son un ecosistema único en el Perú. Las pampas se inundan periódicamente y los pequeños bosques de árboles con variada vida vegetal crecen en grupos aislados en la llanura.
 Si usted está buscando una experiencia más profunda del Amazonas, el Centro de Investigaciones Tambopata le convendrá. Después de pasar la primera noche en el Refugio Amazonas, llega al más grande conjunto de guacamayos conocido en el segundo día de su viaje y explorar los senderos del Centro de Investigación de Tambopata en el corazón de la Reserva Nacional Tambopata.`,
 				additionalData: {
@@ -44,7 +44,7 @@ Si usted está buscando una experiencia más profunda del Amazonas, el Centro de
 				title:'Manu Reserve',
 				shortTitle:'Manu Reserve',
 				subtitle:'The famous Salkantay trek to Machu Picchu is named among the top 25 best excursions in the world by National Geographic Adventure Travel Magazine.',
-				url_image:'images/tours/tambopata/salkantay_1_portrait.jpg',
+				url_image:'https://res.cloudinary.com/dlia7phdx/image/upload/v1551324785/SAS/Manu_Reserve.jpg',
 				description:`The Salkantay Trek is an ancient and remote footpath located in the same region as the Inca Trail where snowcapped mountains collide with lush tropical rain forests.`,
 				additionalData: {
 					include: [
