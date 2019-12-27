c48d2d3e60648c9693abcfce2aab2abf2bce189a
Osmar Reyes <osmarreyesst@gmail.com>
2019-12-27 12:38:26 -0500

    add splash spanish data

--- app/data/splash/index.es.js
+++ app/data/splash/index.es.js
@@ -1,7 +1,7 @@
 export default [
     {
-        title: 'Shore excursions',
-        subtitle: 'from Salaverry Cruise terminal',
+        title: 'Excursiones en tierra',
+        subtitle: 'desde terminal de cruceros, Salaverry',
         description: 'lorem ipsum',
         img: {
             portrait: 'https://res.cloudinary.com/dvv4qgnka/image/upload/v1576877525/SAS/salaberry_splash_pt.jpg',
@@ -11,43 +11,99 @@ export default [
             {
                 destination: 'Marina',
                 title: 'Oceania Cruises',
-                date: 'January 9, Thursday.'
+                date: 'Enero 9, Jueves.'
             },
             {
                 destination: 'Volendam',
                 title: 'Holland America',
-                date: 'January 15, Wednesday.'
+                date: 'Enero 15, Miercoles.'
             },
             {
                 destination: 'Silver Shadow',
                 title: 'Silver Sea',
-                date: 'February 4, Tuesday.'
+                date: 'Febrero 4, Martes.'
             },
             {
                 destination: 'Insignia',
                 title: 'Oceania Cruises',
-                date: 'February 11, Tuesday.'
+                date: 'Febrero 11, Martes.'
             },
             {
                 destination: 'Ms Zaandam',
                 title: 'Holland America',
-                date: 'March 27, Friday.'
+                date: 'Marzo 27, Viernes.'
             },
             {
                 destination: 'Seven Seas Navigator',
                 title: 'Regent Seven Seas',
-                date: 'April 26, Sunday.'
+                date: 'Abril 26, Domingo.'
             },
             {
                 destination: 'Seven Seas Mariner',
                 title: 'Regent Seven Seas',
-                date: 'November 12, Thursday.'
+                date: 'Noviembre 12, Jueves.'
             },
             {
                 destination: 'Marina',
                 title: 'Oceania Cruises',
-                date: 'December 17, Thursday.'
+                date: 'Diciembre 17, Jueves.'
             },
+        ]
+    },
+    {
+        title: 'Excursiones en tierra',
+        subtitle: 'desde terminal de cruceros, Callao',
+        description: 'lorem ipsum',
+        img: {
+            portrait: 'https://res.cloudinary.com/dvv4qgnka/image/upload/v1577414817/SAS/callao_port_pt.jpg',
+            landscape: 'https://res.cloudinary.com/dvv4qgnka/image/upload/v1577414817/SAS/callao_port.jpg'
+        },
+        itinerary: [
+            {destination: "Marina ", title: "Oceania Cruises ", date: "Enero 8."},
+            {destination: "Volendam ", title: "Holland America ", date: "Enero 16, 17."},
+            {destination: "ISLAND PRINCESS ", title: "Princess Cruises ", date: "Enero 21, 22."},
+            {destination: "COSTA DELICIOZA ", title: "Costa Cruises ", date: "Enero 30."},
+            {destination: "Silver Shadow ", title: "Silver Sea ", date: "Febrero 5, 6."},
+            {destination: "Insignia ", title: "Oceania Cruises ", date: "Febrero 12."},
+            {destination: "MSC MAGNIFICA ", title: "MSC Cruises ", date: "Febrero 15."},
+            {destination: "BALMORAL ", title: "Fred. Olsen Cruises ", date: "Febrero 18."},
+            {destination: "QUEEN VICTORIA ", title: "Cunard ", date: "Marzo 5, 6."},
+            {destination: "MARINA ", title: "Oceania Cruises ", date: "Marzo 15."},
+            {destination: "CELEBRITY ECLIPSE ", title: "Celebrity Cruises ", date: "Marzo 19."},
+            {destination: "Star ", title: "Norwegian Cruise Line ", date: "Marzo 20."},
+            {destination: "AZAMARA PURSUIT ", title: "Azamara Club Cruises ", date: "Marzo 23 (Lunes)."},
+            {destination: "Ms Zaandam ", title: "Holland America ", date: "Marzo 25, 26."},
+            {destination: "KONINGSDAM ", title: "Holland America ", date: "Abril 16."},
+            {destination: "Seven Seas Navigator ", title: "Regent Seven Seas ", date: "Abril 24, 25."},
+            {destination: "SEA PRINCESS ", title: "Princess Cruises ", date: "Agosto 3, 4. (Lunes)."},
+            {destination: "Westerdam ", title: "Holland America ", date: "October 25."},
+            {destination: "Seven Seas Mariner ", title: "Regent Seven Seas ", date: "Noviembre 13, 14."},
+            {destination: "CORAL PRINCESS ", title: "Princess Cruises ", date: "Noviembre 27."},
+            {destination: "PACIFIC PRINCESS ", title: "Princess Cruises ", date: "Diciembre 10, 11."},
+            {destination: "Marina ", title: "Oceania Cruises ", date: "Diciembre 18, 19."}
+        ]
+    },
+    {
+        title: 'Excursiones en tierra',
+        subtitle: 'desde Terminal portuario Paracas, Pisco',
+        description: 'lorem ipsum',
+        img: {
+            portrait: 'https://res.cloudinary.com/dvv4qgnka/image/upload/v1577419105/SAS/paracas_splash_pt.jpg',
+            landscape: 'https://res.cloudinary.com/dvv4qgnka/image/upload/v1577419105/SAS/paracas_splash.jpg'
+        },
+        itinerary: [
+            {destination: "Volendam ", title: "Holland America ", date: "Enero 18."},
+            {destination: "Island Princess ", title: "Princess Cruises ", date: "Enero 23."},
+            {destination: "Insignia ", title: "Oceania Cruises ", date: "Febrero 13."},
+            {destination: "Balmoral ", title: "Fred. Olsen Cruises ", date: "Febrero 17."},
+            {destination: "Marina ", title: "Oceania Cruises ", date: "Marzo 14."},
+            {destination: "Star ", title: "Norwegian Cruise Line ", date: "Marzo 19."},
+            {destination: "Azamara Pursuit ", title: "Azamara Club Cruises ", date: "Marzo 22."},
+            {destination: "Sea Princess ", title: "Princess Cruises ", date: "Agosto 5."},
+            {destination: "Seven Seas Mariner ", title: "Regent Seven Seas ", date: "Noviembre 15."},
+            {destination: "Coral Princess ", title: "Princess Cruises ", date: "Noviembre 29."},
+            {destination: "Pacific Princess ", title: "Princess Cruises ", date: "Diciembre 9."},
+            {destination: "Marina ", title: "Oceania Cruises ", date: "Diciembre 20."}
         ]
     }
 ]
